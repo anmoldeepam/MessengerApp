2e15309ff6e7fd07102ed977305e268d8dfb3290
AnmolDeep <amanmoldeep@gmail.com>
2024-03-14 13:19:35 +0100

    Edit Working ( Before implementing Add Image Feature )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.fxn769:pix:1.5.3")
+    implementation("com.iceteck.silicompressorr:silicompressor:2.2.3")
 }
